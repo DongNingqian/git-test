dfaf003c9dae7025710f2e51fccf40927fb24371
DongNingqian <DongNingqian123>
2021-06-29 15:18:20 +0800

    First commit

---
+++ src/main/java/com/atguigu/Git.java
@@ -0,0 +1,16 @@
+package com.atguigu;
+
+/**
+ * @author DongNingqian
+ * @data 2021-06-29-15:10
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Hello, git!");
+        System.out.println("Hello, git!");
+        System.out.println("Hello, git!");
+        System.out.println("Hello, git!");
+        System.out.println("Hello, git!");
+        System.out.println("Hello, git!");
+    }
+}
